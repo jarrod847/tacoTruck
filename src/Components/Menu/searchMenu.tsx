d202c0648ac816c54bd2fb95e71a8c47084b2184
jarrodskahill <jarrodskahill@gmail.com>
2022-02-25 01:20:54 -0800

    moved get request to nav bar to get trucks on inital load and avoid app file size

--- src/Components/Menu/searchMenu.tsx
+++ src/Components/Menu/searchMenu.tsx
@@ -4,16 +4,6 @@ import { useRecoilState } from "recoil";
 import TrucksAtom from "../../recoil/atoms/trucksAtoms";
 
 const SearchMenu = () => {
-  const [trucks, setTrucks] = useRecoilState(TrucksAtom);
-  useEffect(() => {
-    Axios.get("https://my.api.mockaroo.com/locations.json?key=a45f1200").then(
-      (res) => {
-        setTrucks(res.data);
-      }
-    );
-  }, []);
-
-  console.log(trucks);
   return <div></div>;
 };
 
--- src/Components/nav/navbar.tsx
+++ src/Components/nav/navbar.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import truck from "../../imgs/truck.png";
+import TrucksAtom from "../../recoil/atoms/trucksAtoms";
 import { NavBarDiv } from "../styles/navStyles";
 
 const NavBar = () => {
+  const [trucks, setTrucks] = useRecoilState(TrucksAtom);
+  useEffect(() => {
+    Axios.get("https://my.api.mockaroo.com/locations.json?key=a45f1200").then(
+      (res) => {
+        setTrucks(res.data);
+      }
+    );
+  }, []);
+
+  console.log(trucks);
   return (
     <NavBarDiv>
       <img src={truck} />
